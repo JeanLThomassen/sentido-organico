56907ffcb02294fd4a9e8f1e654d94910a651ecb
Jean Thomassen <jeanthomassen.04@gmail.com>
2026-08-11 20:50:07 -0300

    Servicios en actualizacion

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    header.classList.add('header-activo');
-  } else {
-    header.classList.remove('header-activo');
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > 50) {
+//     header.classList.add('header-activo');
+//   } else {
+//     header.classList.remove('header-activo');
+//   }
+// });
 
 import { initSlider } from './components/slider.js';
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    header.classList.add('header-activo');
-  } else {
-    header.classList.remove('header-activo');
-  }
-});
 
 const sliderEl = document.getElementById('slider');
 if (sliderEl) initSlider(sliderEl);
