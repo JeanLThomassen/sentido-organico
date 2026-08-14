07b5ee60b3a8b940158428b50977b54b11092dec
Jean Thomassen <jeanthomassen.04@gmail.com>
2026-08-13 21:27:56 -0300

    Funcionalidad del form 80%

--- js/components/form.js
+++ js/components/form.js
@@ -1,24 +1,43 @@
 export function initBookingForm(root) {
-    const nextBtnForm = root.querySelector('nextBtn-Form');
-    const form_panel = root.querySelectorAll('.form-panel');
+    const step = root.querySelectorAll('.step');
+    const nextBtnForm = root.querySelector('#nextBtn');
+    const prevBtnForm = root.querySelector('#prevBtn'); 
+    const form_panel = root.querySelectorAll('.form_panel');
 
     let currentStep = 0;
-    const step = Array.from(form_panel.children);
-    
-    function update(){
-        const stepOn = step[0].getBoundingClientRect().display;
-        track.style.display = 'display: block';
+    const steps = Array.from(step);
+    const stepPanel = Array.from(form_panel);
+
+    function update(index) {
+        const stepPanelOn = stepPanel[index];
+        const stepOn = steps[index];
+
+        stepPanel.forEach((_, i) => {
+            stepPanel[i].classList.remove('form_panel--active');
+            steps[i].classList.remove('step--selected');
+        });
+
+        stepPanelOn.classList.add('form_panel--active');
+        stepOn.classList.add('step--selected');
     }
 
-    function goToNextStep(index) {
-        currentStep = (index);
-        updateStep();
+    function goToStep(index) {
+        const maxIndex = stepPanel.length - 1;
+
+        if (index > maxIndex) index = maxIndex;
+        if (index < 0) index = 0;
+
+        currentStep = index;
+        update(currentStep);
     }
 
-    const next = () => goToNextStep(currentStep + 1);
+    const next = () => goToStep(currentStep + 1);
+    const prev = () => goToStep(currentStep - 1); 
+
     nextBtnForm.addEventListener('click', next);
+    prevBtnForm.addEventListener('click', prev); 
 
-    update();
+    update(currentStep);
 
-    return {next, goToNextStep};
+    return { next, prev, goToStep };
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+import {initBookingForm} from './components/form.js';
 import { initSlider } from './components/slider.js';
 
 const sliderEl = document.getElementById('slider');
 if (sliderEl) initSlider(sliderEl);
+
+const bookingFormEl = document.getElementById('bookingForm');
+if (bookingFormEl) initBookingForm(bookingFormEl);
