8d08963c92b38dfa9bdc144a57d618ed1286c256
Jean Thomassen <jeanthomassen.04@gmail.com>
2026-09-11 14:27:55 -0300

    error en calendar.js solucionado

--- js/components/calendar.js
+++ js/components/calendar.js
@@ -95,7 +95,7 @@ export function initCalendar(root) {
         const isoDate = selectedDate.toISOString().split('T')[0];
 
         try {
-            const response = await fetch(`http://localhost:3000/api/disponibilidad?date=${isoDate}`);
+            const response = await fetch(`/api/disponibilidad?date=${dateString}`);
             const data = await response.json();
 
             timesContainer.innerHTML = '';
